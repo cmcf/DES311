08ecb59e7c3bed25f4c3a1295a639aa4d2ca0749
Clare-M <64334337+cmcf@users.noreply.github.com>
2024-02-25 17:03:35 +0000

    Updated room

--- DES311/Assets/Scripts/Rifle.cs
+++ DES311/Assets/Scripts/Rifle.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Build;
 using UnityEngine;
 
 public class Rifle : Weapon
@@ -36,3 +33,4 @@ public class Rifle : Weapon
     }
 
 }
+
