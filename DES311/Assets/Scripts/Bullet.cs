35efd8657c88c03573fa365dbfa768188f345291
Clare-M <64334337+cmcf@users.noreply.github.com>
2024-02-29 22:01:24 +0000

    Fire projectile hit effect

--- DES311/Assets/Scripts/Bullet.cs
+++ DES311/Assets/Scripts/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     [SerializeField] float damageAmount = 15f;
     Enemy enemy;
     [SerializeField] float destroyDelay = 3f;
+    [SerializeField] GameObject hitEffect;
+    [SerializeField] float destroyDelayAfterCollision = 0.2f;
     void Start()
     {
         initialPosition = transform.position;
@@ -21,7 +23,18 @@ public class Bullet : MonoBehaviour
         {
             // Apply damage to the enemy
             other.GetComponent<Enemy>().Damage(damageAmount);
+
+            if (hitEffect!= null)
+            {
+                // Instantiate the hit effect at the position of the collision
+                Instantiate(hitEffect, transform.position, Quaternion.identity);
+            }
+
             Destroy(gameObject);
+        }
+        else
+        {
+            Invoke(nameof(DestroyBullet), destroyDelayAfterCollision);
         }
     }
 
