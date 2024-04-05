22caadcf9f5ce04262c0640df1c8c4a655655113
Clare-M <64334337+cmcf@users.noreply.github.com>
2024-04-05 18:49:25 +0100

    SFX On/Off settings

--- DES311/Assets/Scripts/Audio/AudioManager.cs
+++ DES311/Assets/Scripts/Audio/AudioManager.cs
@@ -1,13 +1,29 @@
 using System;
 using UnityEngine.Audio;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance = null;
     public Sound[] sounds;
+    private bool sfxEnabled = true; // New field to store SFX enable/disable state
 
-     void Awake()
-     {
+    void Awake()
+    {
+        // Singleton pattern to ensure only one instance exists
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         foreach (Sound s in sounds)
         {
             // Adds audio source to game object with sound
@@ -16,14 +32,58 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+
+            // Apply stored SFX status
+            ApplySFXStatus();
+
         }
-     }
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        foreach (Sound s in sounds)
+        {
+            if (s.type == SoundType.Music)
+            {
+                s.source.volume = volume;
+            }
+        }
+    }
+
+    public void ToggleSFX(bool enabled)
+    {
+        sfxEnabled = enabled;
+        foreach (Sound s in sounds)
+        {
+            if (s.type == SoundType.SFX)
+            {
+                s.source.volume = enabled ? s.volume : 0.0f;
+            }
+        }
+    }
+    public void ApplySFXStatus()
+    {
+        bool enabled = PlayerPrefs.GetInt("SFXEnabled", 1) == 1; // Default to enabled if not set
+        ToggleSFX(enabled);
+    }
+
+    public bool IsSFXEnabled()
+    {
+        return sfxEnabled;
+    }
 
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = System.Array.Find(sounds, sound => sound.name == name);
         // Plays audio clip
-        s.source.Play();
+        if (s != null && s.source != null)
+        {
+            s.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+        }
     }
 }
 
--- DES311/Assets/SettingsFunctions.cs
+++ DES311/Assets/SettingsFunctions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsFunctions : MonoBehaviour
 {
@@ -9,8 +10,114 @@ public class SettingsFunctions : MonoBehaviour
     [SerializeField] Canvas settingsCanvas;
     [SerializeField] Canvas deathCanvas;
 
+    [Header("Buttons")]
+    [SerializeField] Button vibrationOn; // Reference to the vibration button
+    [SerializeField] Button vibrationOff; // Reference to the vibration button
+    [SerializeField] Button fixedButton; // Reference to the fixed joystick button
+    [SerializeField] Button dynamicButton; // Reference to the dynamic joystick button
+    [SerializeField] Button sfxOnButton;
+    [SerializeField] Button sfxOffButton;
+
+    public bool sfxEnabled = true;
+    bool vibrationEnabled = true; // Default vibration state
+    bool isFixedJoystickSelected = true; // Default joystick type selection
+
+
+    void LoadSettings()
+    {
+        // Load saved settings (vibration and SFX state) from PlayerPrefs
+        vibrationEnabled = PlayerPrefs.GetInt("VibrationEnabled", 1) == 1;
+        sfxEnabled = PlayerPrefs.GetInt("SFXEnabled", 1) == 1;
+        isFixedJoystickSelected = PlayerPrefs.GetInt("IsFixedJoystickSelected", 1) == 1;
+    }
+
+    void SaveSettings()
+    {
+        // Save current settings (vibration and SFX state) to PlayerPrefs
+        PlayerPrefs.SetInt("VibrationEnabled", vibrationEnabled ? 1 : 0);
+        PlayerPrefs.SetInt("SFXEnabled", sfxEnabled ? 1 : 0);
+        PlayerPrefs.SetInt("IsFixedJoystickSelected", isFixedJoystickSelected ? 1 : 0);
+    }
+
+
+    void UpdateButtonAppearance()
+    {
+        // Update appearance of vibration button
+        if (vibrationEnabled)
+        {
+            vibrationOn.GetComponent<Image>().color = Color.white;
+            vibrationOff.GetComponent<Image>().color = Color.grey;
+        }
+        else if (!vibrationEnabled)
+        {
+            vibrationOn.GetComponent<Image>().color = Color.grey;
+            vibrationOff.GetComponent<Image>().color = Color.white;
+        }
+        // Update appearance of joystick type buttons
+        if (isFixedJoystickSelected)
+        {
+            fixedButton.GetComponent<Image>().color = Color.white;
+            dynamicButton.GetComponent<Image>().color = Color.grey;
+        }
+        else if (!isFixedJoystickSelected)
+        {
+            fixedButton.GetComponent<Image>().color = Color.grey;
+            dynamicButton.GetComponent<Image>().color = Color.white;
+        }
+        // Update appearance of SFX button
+        if (sfxEnabled)
+        {
+            sfxOnButton.GetComponent<Image>().color = Color.white;
+            sfxOffButton.GetComponent<Image>().color = Color.grey;
+        }
+        else if (!sfxEnabled)
+        {
+            sfxOnButton.GetComponent<Image>().color = Color.grey;
+            sfxOffButton.GetComponent<Image>().color = Color.white;
+        }
+    }
+
+    public void ToggleVibration()
+    {
+        vibrationEnabled = !vibrationEnabled;
+        SaveSettings();
+        UpdateButtonAppearance();
+    }
+
+    public void SelectFixedJoystick()
+    {
+        fixedButton.interactable = false;
+        dynamicButton.interactable = true;
+        SaveSettings();
+        UpdateButtonAppearance();
+    }
+
+    public void SelectDynamicJoystick()
+    {
+        fixedButton.interactable = true;
+        dynamicButton.interactable = false;
+        SaveSettings();
+        UpdateButtonAppearance();
+    }
+
+    public void ToggleSFX()
+    {
+        sfxEnabled = !sfxEnabled;
+        SaveSettings();
+        UpdateButtonAppearance();
+    }
+
     void Start()
     {
+        // Load saved settings
+        LoadSettings();
+        LoadSFXState();
+
+        if (vibrationOn || vibrationOff != null)
+        {
+            UpdateButtonAppearance();
+        }
+
         if (settingsCanvas != null && deathCanvas != null)
         {
             settingsCanvas.enabled = false;
@@ -21,15 +128,33 @@ public class SettingsFunctions : MonoBehaviour
             return;
         }
     }
-    public void ApplyButton()
+
+    // Method to load the saved SFX state from PlayerPrefs
+    void LoadSFXState()
     {
-        PlayButtonSFX();
-        Settings.instance.ApplyVibration();
+        // Default to sfx enabled if not set
+        sfxEnabled = PlayerPrefs.GetInt("SFXEnabled", 1) == 1; 
     }
-    public void DisableButton()
+
+    // Method to save the current SFX state to PlayerPrefs
+    void SaveSFXState()
+    {
+        PlayerPrefs.SetInt("SFXEnabled", sfxEnabled ? 1 : 0);
+    }
+
+    public void ApplyVibrationButton()
     {
         PlayButtonSFX();
+        vibrationEnabled = true;
+        Settings.instance.ApplyVibration();
+        UpdateButtonAppearance();
+    }
+    public void DisableVibrationButton()
+    {
+        PlayButtonSFX();
+        vibrationEnabled = false;
         Settings.instance.DisableVibration();
+        UpdateButtonAppearance();
     }
 
     public void BackButton()
@@ -37,7 +162,6 @@ public class SettingsFunctions : MonoBehaviour
         PlayButtonSFX();
         SceneManager.LoadScene("MainMenu");
     }
-
     public void PlayGame()
     {
         PlayButtonSFX();
@@ -51,7 +175,10 @@ public class SettingsFunctions : MonoBehaviour
 
     void PlayButtonSFX()
     {
-        AudioSource.PlayClipAtPoint(buttonSFX, transform.position);
+        if (sfxEnabled)
+        {
+            AudioSource.PlayClipAtPoint(buttonSFX, transform.position);
+        }
     }
     public void PauseSettingsButton()
     {
@@ -70,14 +197,31 @@ public class SettingsFunctions : MonoBehaviour
     public void FixedButton()
     {
         PlayButtonSFX();
+        isFixedJoystickSelected = true;
         Settings.instance.ApplyFixedJoystick();
+        UpdateButtonAppearance();
     }
 
     public void DynamicButton()
     {
         PlayButtonSFX();
+        isFixedJoystickSelected = false;
         Settings.instance.ApplyDynamicJoystick();
+        UpdateButtonAppearance();
+    }
+    public void EnableSFXButton()
+    {
+        PlayButtonSFX();
+        sfxEnabled = true;
+        SaveSFXState();
+        UpdateButtonAppearance();
     }
 
-
+    public void DisableSFXButton()
+    {
+        PlayButtonSFX();
+        sfxEnabled = false;
+        SaveSFXState();
+        UpdateButtonAppearance();
+    }
 }
